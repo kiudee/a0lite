d9054e1ad8b8c10bf73517d9b4a7ae15d07a6264
Karlson Pfannschmidt <kiudee@mail.upb.de>
2020-09-08 02:27:51 +0200

    Refactor out update_policy and update_value

--- search/uct.py
+++ search/uct.py
@@ -7,7 +7,7 @@ import numpy as np
 from search.util import cp
 
 FPU = 0.0
-FPU_ROOT = 1.0
+FPU_ROOT = 0.0
 
 
 class UCTNode(object):
@@ -18,7 +18,7 @@ class UCTNode(object):
         self.parent = parent  # Optional[UCTNode]
         self.children = OrderedDict()  # Dict[move, UCTNode]
         self.prior = prior  # float
-        if parent is None:
+        if parent is None or parent.parent is None:
             self.total_value = FPU_ROOT  # float
         else:
             self.total_value = FPU
@@ -73,22 +73,57 @@ def rel_entropy_max(Q, p):
     return p * np.exp(Q) / denominator
 
 
-class RENTSNode(UCTNode):
+def logit(p):
+    return -np.log(1 / p - 1)
 
+
+def expit(x):
+    return 1 / (1 + np.exp(-x))
+
+
+class RENTSNode(UCTNode):
     def __init__(
         self,
         board=None,
         parent=None,
         move=None,
         prior=0,
-        discount_factor=0.99,
-        eps=0.001,
+        discount_factor=1.0,
+        eps=0.01,
+        prior_visits=1,
+        value_prior_strength=0.01,
+        maximum_exploration=1.0,
+        init_strategy="VplusP",
+        fpu_value=0.0,
+        fpu_strategy="reduction",
+        fpu_value_at_root=0.1,
+        fpu_strategy_at_root="absolute",
         **kwargs
     ):
         super().__init__(board=board, parent=parent, move=move, prior=prior, **kwargs)
         self.discount_factor = discount_factor
         self.eps = eps
         self.policy = prior
+        self.prior_visits = prior_visits
+        self.maximum_exploration = maximum_exploration
+        self.init_strategy = init_strategy
+        self.fpu_strategy = fpu_strategy
+        self.fpu_strategy_at_root = fpu_strategy_at_root
+        # self.total_value = -np.log(1/min(max(prior, 0.01), 0.99) - 1) * value_prior_strength
+        if init_strategy == "fpu":
+            is_root = parent is None or parent.parent is None
+            if is_root:
+                fpu = fpu_value_at_root
+            else:
+                fpu = fpu_value
+            if (is_root and self.fpu_strategy_at_root == "reduction") or (
+                not is_root and self.fpu_strategy == "reduction"
+            ):
+                self.total_value = -fpu
+            else:
+                self.total_value = fpu
+        elif init_strategy == "VplusP" or init_strategy == "logP":
+            self.initial_value = 0
 
     def add_child(self, move, prior):
         self.children[move] = RENTSNode(parent=self, move=move, prior=prior)
@@ -96,54 +131,85 @@ class RENTSNode(UCTNode):
     def best_child(self, C):
         n_children = len(self.children)
         if n_children == 1:
-            return list(self.children.values())[0]
-        visits = [child.number_visits + 1 for child in self.children.values()]
-        # sum_visits = np.sum(visits)
-        # if sum_visits == 0:
-        #     p = np.array([child.policy for child in self.children.values()])
-        #     p /= p.sum()
-        #     return list(self.children.values())[int(np.random.choice(n_children, size=1, p=p))]
-        lambda_s = self.eps * n_children / np.log(np.sum(visits))
-        Q, p = np.array(
-            [(child.Q(), child.policy) for child in self.children.values()]
-        ).T
-        #print("lambda_s = {}".format(lambda_s))
-        #print("Q = {}".format(Q))
-        #print("p = {}".format(p))
+            child = list(self.children.values())[0]
+            return child
+        p = [child.policy for child in self.children.values()]
+        return list(self.children.values())[
+            int(np.random.choice(n_children, size=1, p=p))
+        ]
+
+    def update_value(self, value, is_leaf):
+        is_root = self.parent is None or self.parent.parent is None
+        if is_leaf:
+            if (
+                (
+                    (is_root and self.fpu_strategy_at_root == "reduction")
+                    or (not is_root and self.fpu_strategy == "reduction")
+                )
+                or self.init_strategy == "logP"
+                or self.init_strategy == "VplusP"
+            ):
+                self.total_value += value
+            else:
+                self.total_value = value
+        else:
+            self.total_value += value
+
+    def update_policy(self, Q, p, visits):
+        n_children = len(self.children)
+        sum_visits = np.sum(visits)
+        if sum_visits == 0:
+            lambda_s = 1.0
+        else:
+            lambda_s = np.clip(
+                self.eps * n_children / np.log(sum_visits + self.prior_visits), 0.0, 1.0
+            )
+        lambda_s = min(self.maximum_exploration, lambda_s)
         max_arg = rel_entropy_max(Q, p)
         new_p = (1 - lambda_s) * max_arg + lambda_s / n_children
-        #print(new_p)
-        if np.any(new_p < 0.0):
-            print("lambda_s = {}".format(lambda_s))
-            print("Q = {}".format(Q))
-            print("p = {}".format(p))
-            print("new_p = {}".format(new_p))
         new_p /= new_p.sum()
         for i, child in enumerate(self.children.values()):
             child.policy = new_p[i]
-        return list(self.children.values())[int(np.random.choice(n_children, size=1, p=new_p))]
 
     def backup(self, value_estimate: float):
         current = self
-        # Child nodes are multiplied by -1 because we want max(-opponent eval)
-        #print("\nInitial estimate: {}".format(value_estimate))
+        if self.init_strategy == "VplusP":
+            value_estimate = np.clip(value_estimate, -1 + 1e-10, 1 - 1e-10)
+            value_estimate = np.arctanh(value_estimate)
+            self.initial_value = value_estimate
+            n_children = len(self.children)
+            for child in self.children.values():
+                prior = min(max(child.prior, 0.001), 0.999)
+                child.total_value = self.initial_value + np.arctanh(prior - 1 / n_children)
+        elif self.init_strategy == "logP":
+            value_estimate = np.clip(value_estimate, -1 + 1e-10, 1 - 1e-10)
+            value_estimate = np.arctanh(value_estimate)
+            for child in self.children.values():
+                prior = min(max(child.prior, 0.001), 0.999)
+                child.total_value = np.log(prior)
+        # print("\nValue estimate: {}".format(value_estimate))
         while current.parent is not None:
             current.number_visits += 1
-            #print("Value before: {}".format(current.total_value))
-            current.total_value += -value_estimate * self.discount_factor
-            #print("Value after: {}".format(current.total_value))
+            val = -value_estimate * current.discount_factor
+            current.update_value(val, is_leaf=self == current)
             current = current.parent
-            Q, p = np.array(
-                [(child.Q(), child.policy) for child in current.children.values()]
+            Q, p, visits = np.array(
+                [
+                    (child.Q(), child.policy, child.number_visits)
+                    for child in current.children.values()
+                ]
             ).T
-            #print("Q: {}".format(Q))
-            #print("p: {}".format(p))
             value_estimate = rel_entropy_value(Q, p)
-            #print("Value: {}".format(value_estimate))
+            current.update_policy(Q, p, visits)
         current.number_visits += 1
 
 
 def get_best_move(root):
+    if isinstance(root, RENTSNode):
+        bestmove, node = max(
+            root.children.items(), key=lambda item: (item[1].policy, item[1].Q())
+        )
+    else:
     bestmove, node = max(
         root.children.items(), key=lambda item: (item[1].number_visits, item[1].Q())
     )
@@ -198,7 +264,8 @@ def UCT_search(
 
     bestmove, node, score = get_best_move(root)
     if send is not None:
-        for nd in sorted(root.children.items(), key=lambda item: item[1].number_visits):
+        root.best_child(1.0)
+        for nd in sorted(root.children.items(), key=lambda item: item[1].policy):
             send(
                 "info string {} {} \t(P: {}%) \t (Pol: {}%) \t(Q: {})".format(
                     nd[1].move,
